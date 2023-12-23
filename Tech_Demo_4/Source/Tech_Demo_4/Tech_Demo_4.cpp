1bd61f8d3a7d2376e638a566a2aa41de2d2042a4
Nathan <nat0tpsn@gmail.com>
2023-12-23 21:08:54 +0000

    Push

--- Tech_Demo_4/Source/Tech_Demo_4/Tech_Demo_4.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Tech_Demo_4.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tech_Demo_4, "Tech_Demo_4" );
